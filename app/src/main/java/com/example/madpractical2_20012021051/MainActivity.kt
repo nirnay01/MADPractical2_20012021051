70f70da01992a80b169735b7cc6f56297bd84257
sidhhii <siddhitoliya20@gnu.ac.in>
2022-08-22 12:29:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madpractical2_20012021051/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.madpractical2_20012021051
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    private val TAG="MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i(TAG, "onCreate:OnCreate method is called ")
+        Toast.makeText(this,"onCreate:OnCreate method is called",Toast.LENGTH_LONG).show()
+        Snackbar.make(
+            findViewById(R.id.myCoordinatorLayout),
+            R.string.id,
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "OnStart:OnStart method is called ")
+        Toast.makeText(this,"onStart:OnStart method is called",Toast.LENGTH_LONG).show()
+        Snackbar.make(
+            findViewById(R.id.myCoordinatorLayout),
+            R.string.id,
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart:OnRestart method is called ")
+        Toast.makeText(this,"OnRestart:OnRestart method is called",Toast.LENGTH_LONG).show()
+        Snackbar.make(
+            findViewById(R.id.myCoordinatorLayout),
+            R.string.id,
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume:onResume method is called ")
+        Toast.makeText(this,"onResume:onResume method is called",Toast.LENGTH_LONG).show()
+        Snackbar.make(
+            findViewById(R.id.myCoordinatorLayout),
+            R.string.id,
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+}
